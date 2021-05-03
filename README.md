53b9fee8161ec0cafe27d43e73f86659fb13e221
sudo-julia <jlearning@tuta.io>
2021-05-03 13:10:35 -0700

    environment variable support for statusfile location

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # polybar-mullvad-status
 
 A script that shows if Mullvad is connected, connecting or disconnected.
+
 Notifications are builtin, unlike the version I submitted to [polybar-scripts](https://github.com/polybar/polybar-scripts).
+
+The script checks for the `POLYBAR_MULLVAD_STATUS` variable at runtime, which
+should be the location you want to write the statusfile to. The default is
+`/tmp/mullvad_status.last`.
 
 ![vpn-mullvad-status](screenshots/1.png)
 ![vpn-mullvad-status](screenshots/2.png)
--- polybar-mullvad-status.sh
+++ polybar-mullvad-status.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 
 # format mullvad status to a better looking printable format
 mullvad_status="$( mullvad status | cut -d' ' -f3 )"
 
 # log the most recent mullvad status
-status_file="${XDG_CONFIG_HOME}/polybar/.mullvad_status.last"
+if [[ "${POLYBAR_MULLVAD_STATUS}" ]]; then
+    status_file="${POLYBAR_MULLVAD_STATUS}"
+else
+    status_file="/tmp/mullvad_status.last"
+fi
 
 # create the status file if it doesn't exist
 if [[ ! -s "${status_file}" ]]; then
-  echo "${mullvad_status}" > "${status_file}"
+    echo "${mullvad_status}" > "${status_file}"
 fi
+
 last_status="$( cat "${status_file}" )"
 
+# TODO make a way to display the notification if the status hasn't changed (at launch)
 send_notification() {
-  # if the vpn's status changed, notify the user
-  if [[ "${last_status}" != "$1" ]]; then
-    echo "$1" | tee "${status_file}" \
-    && notify-send "$1"
-  fi
+    # if the vpn's status changed, notify the user
+    if [[ "${last_status}" != "$1" ]]; then
+        echo "$1" | tee "${status_file}" \
+        && notify-send "$1"
+    fi
 }
 
 send_notification "VPN $mullvad_status"
